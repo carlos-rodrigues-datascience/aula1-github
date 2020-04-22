c9a6c72adb51d82d8a98c66e0a259df3d85f586c
Carlos Rodrigues <carlos.rodrigues@engenharia.ufjf.br>
2020-04-22 06:31:00 -0300

    Acrescentada mensagem de bom dia

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
 
 	public static void main(String[] args) {
 		System.out.println("Boa noite!!!");
-
+		System.out.println("boa tarde");
+		System.out.println("Bom dia!!!");
 	}
-
 }
